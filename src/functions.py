e2fd6c448b5afb70505c0170edeb555402a30b56
Marianne2109 <15081@coderacademy.edu.au>
2024-05-02 01:23:48 +1000

    continue working in def functions, creating files, loops

--- src/functions.py
+++ src/functions.py
@@ -5,7 +5,7 @@ import csv
 
 
 
-habits_database_file_path = "src/habits_database.csv"
+habits_database_file_path = "./src/habits_database.csv"
 habits_database_header = "habit_name,habit_log"
 
 
@@ -18,33 +18,32 @@ def main_menu():
     print("4. View Statistics")
     print("5. Quit Application")
     
-    user_selection = int(input("Select an option by entering the menu number: "))
-    return user_selection
-
+    
 user_selection = ""
 
 while user_selection != 5:
-    user_selection = main_menu()    
-    if (user_selection == 1):
+    main_menu()   
+    user_selection = int(input("Enter your choice from the menu number: \n")) 
+    if user_selection == 1:
         create_new_list() 
-    elif (user_selection == 2):
+    elif user_selection == 2:
         my_habits_list()
-    elif (user_selection == 3):
+    elif user_selection == 3:
         habits_database()
-    elif (user_selection == 4):
+    elif user_selection == 4:
         view_statistics()
-    elif (user_selection == 5):
+    elif user_selection == 5:
         print("Are you leaving?")
     else:
-        print("Invalid option, please enter an option from 1-5")
-
-print("Sad to see you go, see you next time!")
+        print("Invalid option, please try again by entering an option from 1-5")  
 
 
-#Create new list, create function to display option for a new habits list
+
+#Create new list, this function is to display option for a new habits list
 def create_new_list(habit, frequency, log):
     habit_list = {"habit": habit, "frequency": frequency, "log": log}
-    print("Let's create a new List of Habits to track!\n")
+    print("Ready to start? Let's create a new List of Habits to track!\n\n")
+    print("You can check the 'Habits Database' for ideas of habits you can start tracking today")
     add_new_list.append(habit_list)
     add_new_list = input("What habit would you like to start tracking? Please enter: ")
     
@@ -58,3 +57,16 @@ def my_habits_list():
 
 #Database, function to display habits listed in the Database
 def habits_database(csv_path):
+    with open (csv_path, "r") as file:
+        csv_reader = csv.reader(file)
+        header = next (csv_reader)
+        habits_table.field = ["habit", "log"]
+        for row in csv_reader:
+            (
+                habit_name,
+                habit_log,
+            ) = row
+            habit_table.add_row(row)
+        print(f"See the current habits in the database: \n")
+        print(f"{habits_table}\n")
+        
