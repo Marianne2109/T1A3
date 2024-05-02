4de538f666516c5d06c6544f9ac1bf32981c7eca
Marianne2109 <15081@coderacademy.edu.au>
2024-05-02 16:49:18 +1000

    working on create new habit and current list

--- src/functions.py
+++ src/functions.py
@@ -1,12 +1,46 @@
 
 import csv
 
+#External packages
+from prettytable import PrettyTable
+
+
 # Define file path to files used in the application
 
 
 
 habits_database_file_path = "./src/habits_database.csv"
 habits_database_header = "habit_name,habit_log"
+
+
+#Create new list options, make a new list of habits to track
+#enter habit, frequency for tracking and log habit when done
+create_new_list = {
+    "Create New habits list": "create_submenu('new', new_habits_file_path)"       
+}
+
+#My habits list, list of the habits currently being tracked in a table. Log a habit by entering 0 (zero)
+#Option to add or delete habits from the list. 
+my_habits_list = {
+    "View current tracked habits": "display_records('entry', )",
+    "Add habit or log to current list": "add_log_submenu('log', )", 
+    "Delete habit or log from current list": "delete_log_submenu('delete', )", 
+}
+
+#Habit database options, view the current database of habit that can be tracked
+#as a reference point. Add or delete habits.
+habits_database = {
+    "View Habits in database": "habits_database(habits_database_file_path)",
+    "Add new habit to the database": "add_submenu('habit', habits_database_file_path)",
+    "Delete habit": "delete_submenu('habit', habits_database_file_path, habits_database_header)",
+}
+
+
+#View statistics, options to analise the performance of currents tracked habits
+view_statistics = {
+    
+}
+
 
 
 #Main menu function, to navigate through the general options in the menu
@@ -40,6 +74,9 @@ while user_selection != 5:
 
 
 #Create new list, this function is to display option for a new habits list
+#habit:habit name
+#frequency: daily, weekly, fortnightly, monthly
+#log: use 0 (zero) to log a habit when it's done
 def create_new_list(habit, frequency, log):
     habit_list = {"habit": habit, "frequency": frequency, "log": log}
     print("Ready to start? Let's create a new List of Habits to track!\n\n")
@@ -53,20 +90,22 @@ def create_new_list(habit, frequency, log):
     #create_new_list_file.close()
 
 def my_habits_list():
-    print("Here is the list of the habits you are tracking\n")
+    print("Here is the list of the habits you are currently tracking\n")
 
 #Database, function to display habits listed in the Database
-def habits_database(csv_path):
-    with open (csv_path, "r") as file:
-        csv_reader = csv.reader(file)
-        header = next (csv_reader)
-        habits_table.field = ["habit", "log"]
-        for row in csv_reader:
+def habits_database (csv_path):
+    with open ("habits_database.csv", "r") as file:
+        csvreader = csv.reader(file)
+        header = next (csvreader)
+        habits_table = PrettyTable()
+        habits_table.field_name = ["habit", "log"]
+        for row in csvreader:
             (
                 habit_name,
                 habit_log,
             ) = row
-            habit_table.add_row(row)
-        print(f"See the current habits in the database: \n")
-        print(f"{habits_table}\n")
+            habits_table.add_row(row)
+        print("See the current habits stored in the database: \n")
+        print("habits_table\n")
+        input("Press enter to return to the main menu")
         
