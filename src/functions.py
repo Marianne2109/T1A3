826dfb8b8f10931c601006e0af1830d6ea26974a
Marianne2109 <15081@coderacademy.edu.au>
2024-05-06 01:03:20 +1000

    working on bugs in the code

--- src/functions.py
+++ src/functions.py
@@ -1,90 +1,65 @@
 #System Packages
 import csv
-import os 
+import os.path 
 import datetime as datetime
 
 #External Packages
 from prettytable import PrettyTable
 
 
-#print("\n           Welcome to your Habits Tracker \n")
-
-
-
-#Function to create CSV file is it dooesn't exist
-def create_new(file_name):
- if (not os.path.isfile(file_name)):
-    print("File not found, creating new file")
-    create_new = open(file_name, "w")
-    create_new.write("habit name", "start date", "frequency", "mark habit")
-    create_new.close()
+#Function to create a new habit and append to the CSV file
+def add_habits(file_name):
+    print("\n Let's start tracking some habits!\n")
     
-file_name = []
-
-
-
-#Main menu function, this is the main menu of the terminal application. 
-#The user is prompted to enter input depending on what they wish to do.
-def main_menu():
-    print("Please choose from the menu below: \n")
-    print("1. Create new habits") #user is asked to enter one or more habits to save 
-    print("2. Edit, Delete or Mark habit") #Submenu
-    print("3. Activity overview") #shows table with all saved habits
-    print("4. Habits database") 
-    print("5. View Statistics")
-    print("6. Quit Application")
+    #User to input name of a habit 
+    habit_name = input("Enter a habit you would like to start tracking: ")
     
-user_selection = ""
-
-while user_selection != 6:
-    main_menu()   
-    user_selection = int(input("Enter your choice from the menu number: \n")) 
-    if user_selection == 1:
-        create_new(file_name) 
-    elif user_selection == 2:
-        edit_delete_log(file_name)
-    elif user_selection == 3:
-        activity_overview(file_name)
-    elif user_selection == 4:
-        habits_database(file_name)
-    elif user_selection == 5:
-        view_statistics()
-    elif user_selection == 6:
-        print("Are you leaving?")
-    else:
-        print("Invalid option, please try again by entering an option from 1-5")  
+    #User to set a date to start tracking
+    while True:
+        try:  
+            start_date = input("Enter the date you'd like to start tracking this habit (YYYY/MM/DD): ")
+            date = datetime.date.strptime(start_date, format="%Y/%m/%d").date()     
+        except ValueError:
+            print("Invalid date format. Please enter a valid date (YYYY/MM/DD)")
+            continue
+        else:
+            break
+    
+    #User set to specify the frequency they want to check the habit
+    while True:
+        frequency = input("How often would you like to mark this activity (daily or weekly): ")
+        if frequency not in ["daily", "weekly"]:
+            print("Invalid input. Please enter 'daily' or 'weekly'")
+            continue
+        else:
+            break
+    
+    #Append habits to CSV file
+    with open(file_name, "a") as f:
+        writer = csv.writer(f) 
+        writer.writerow = ([habit_name, start_date, frequency, False])
         
-file_name = [habits_database.csv, create_new.csv]
-
-
-def create_new(file_name):
-    print("Now let's start tracking some habits!\n")
-    with open(create_new, "a") as file:
-       writer = csv.writer(file) 
-       writer.writerow = (["habit name", "start date", "frequency", "mark habit"])
-       #habits_number = int(input("Please enter how many habits would you like to add: "))
-        habitname = input("Enter a habit you would like to start tracking: ")
-        print("You can log this activity daily, weekly or monthly, it's up to you!")
-        frequency = input("How often would you like to log this activity: ")
-           
+    
+       
         
-#add one or more habits to CSV file
-#habits variables for habit name, start date and frequency (daily or weekly)
 
+#submenu that will give the option to edit, delete or mark a habit as completed
 def edit_delete_log():
     print("edit, delete or log a habit")
-#submenu that will give the option to edit, delete or mark a habit as completed
+
 
 def activity_overview():
     print("print table with current habits")
+    
 #This function will display all saved habits in a table (habit name and start date)
 
 def habits_database(habits_database):
     print("see list of habit for inspiration")
+    #with open(habits_database, "r") as file:
 #This function will display a csv file with a list of random habits that can be tracked as reference
 #It will have the option to add habit to the list for future reference 
 
-def view_stats():
-    print("view statisics")
+#def view_stats():
+    #print("view statisics")
 #This function will display a list of all habits and their streak
 #User can check the stats for individual habits. 
--- src/main.py
+++ src/main.py
@@ -1,12 +1,53 @@
 
 #System packages
+import csv
+import os.path 
 
 #External packages
 
 #Import from functions.py
-from archive.functions import myhabits_htracker, template_htracker, database_htracker, stats_htracker
+from functions import add_habits, edit_delete_log, activity_overview, habits_database
+
+file_name = "habits.csv"
+
+#Check of CSV exist and create one if it dooesn't 
+if not os.path.isfile(file_name):
+    with open (file_name, "w") as file:
+        file.write("Habit Name, Start Date, Frequency, Mark Habit\n")
 
 
+
+#Main function, run the application includes welcome message
 
 print("Welcome to Habits Tracker\n")
 
+#Main menu function, this is the main menu of the terminal application. 
+#The user is prompted to enter input depending on what they wish to do.
+def main_menu():
+    print("Please choose from the menu below: \n")
+    print("1. Add new habits") #user is asked to enter one or more habits to save 
+    print("2. Edit, Delete or Mark habit") #Submenu
+    print("3. Activity overview") #shows table with all saved habits
+    print("4. Habits database") 
+    #print("5. View Statistics")
+    print("5. Quit Application")
+    
+user_selection = ""
+
+#Main menu loop
+while user_selection != 5:
+    main_menu()   
+    user_selection = int(input("Enter your choice from the menu number: \n")) 
+    if user_selection == 1:
+        add_habits(file_name) 
+    elif user_selection == 2:
+        edit_delete_log(file_name)
+    elif user_selection == 3:
+        activity_overview(file_name)
+    elif user_selection == 4:
+        habits_database()
+    elif user_selection == 5:
+        print("Are you leaving?")
+    else:
+        print("Invalid option, please try again by entering an option from 1-5")  
+        
